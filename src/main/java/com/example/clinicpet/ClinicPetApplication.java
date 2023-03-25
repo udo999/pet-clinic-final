604676f40e9ed35607f63774f745611fb253300f
Udi <zeljkoudovicic@gmail.com>
2023-03-25 12:46:37 +0100

    Initial commit

---
+++ src/main/java/com/example/clinicpet/ClinicPetApplication.java
@@ -0,0 +1,13 @@
+package com.example.clinicpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicPetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicPetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clinicpet/ClinicPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clinicpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicPetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
